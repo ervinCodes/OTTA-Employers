dc4f6c795a14cfa42e1ec3b576ee75f1767b0bd1
Ervin Natareno <ean1207@gmail.com>
2024-11-19 09:55:12 -0800

    Redesigned dashboard with tabs

--- Interior/js/tabs.js
+++ Interior/js/tabs.js
@@ -1,30 +1,68 @@
-const tabs = document.querySelectorAll('[data-tab-target]');
-const activeClass = 'bg-white-gold';
-let activeTab = tabs[0];
+// const tabs = document.querySelectorAll('[data-tab-target]');
+// const activeClass = 'bg-white';
+// const activeText = 'text-federal-blue'
+// let activeTab = tabs[0];
 
-// Select the first tab as the detault
-activeTab.classList.add(activeClass);
-activeTab.classList.add('text-federal-blue');
-document.querySelector('#tab1').classList.remove('hidden');
+// // Select the first tab as the detault
+// activeTab.classList.add(activeClass);
+// activeTab.classList.add(activeText);
+// activeTab.classList.remove('text-white');
+// document.querySelector('#tab1').classList.remove('hidden');
 
-// Add event listener to each tab
-tabs.forEach(tab => {
+// // Add event listener to each tab
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         const targetContent = document.querySelector(tab.dataset.tabTarget);
+
+//         // Hide all tab-content divs
+//         document.querySelectorAll('.tab-content').forEach(content => content.classList.add('hidden'));
+
+//         // Show the content for the clicked tab
+//         targetContent.classList.remove('hidden');
+
+//         //Remove the active class from the previously active tab
+//         activeTab.classList.remove(activeClass);
+//         activeTab.classList.remove(activeText);
+//         activeTab.classList.add('text-white');
+
+//         // Add the active class to the click tab button
+//         activeTab = tab;
+//         tab.classList.add(activeClass)
+//         activeTab.classList.add(activeText);
+//         activeTab.classList.remove('text-white');
+//     })
+// })
+
+// function updateTabSelection(event) {
+//     const tabs = document.querySelectorAll('.tab');
+//     tabs.forEach(tab => {
+//       tab.setAttribute('aria-selected', 'false');
+//     });
+//     event.currentTarget.setAttribute('aria-selected', 'true');
+//   }
+
+//   function selectFirstTabOnLoad() {
+//     const firstTab = document.querySelector('.tab');
+//     if (firstTab) {
+//       firstTab.setAttribute('aria-selected', 'true');
+//     }
+//   }
+
+//   document.querySelectorAll('.tab').forEach(tab => {
+//     tab.addEventListener('click', updateTabSelection);
+//   });
+
+//   document.addEventListener('DOMContentLoaded', selectFirstTabOnLoad);
+
+document.querySelectorAll('.tab').forEach((tab) => {
     tab.addEventListener('click', () => {
-        const targetContent = document.querySelector(tab.dataset.tabTarget);
-
-        // Hide all tab-content divs
-        document.querySelectorAll('.tab-content').forEach(content => content.classList.add('hidden'));
-
-        // Show the content for the clicked tab
-        targetContent.classList.remove('hidden');
-
-        //Remove the active class from the previously active tab
-        activeTab.classList.remove(activeClass);
-        activeTab.classList.remove('text-federal-blue');
-
-        // Add the active class to the click tab button
-        activeTab = tab;
-        tab.classList.add(activeClass)
-        activeTab.classList.add('text-federal-blue');
-    })
-})
+      // Deselect all tabs
+      document.querySelectorAll('.tab').forEach((t) => t.setAttribute('aria-selected', 'false'));
+      // Hide all content
+      document.querySelectorAll('.tab-content').forEach((content) => content.classList.add('hidden'));
+      // Select clicked tab
+      tab.setAttribute('aria-selected', 'true');
+      // Show linked content
+      document.getElementById(tab.getAttribute('aria-controls')).classList.remove('hidden');
+    });
+  });
